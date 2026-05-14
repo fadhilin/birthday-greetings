0d5e8fdea9599d124c661b556760732ab8e6dcc3
Fadhilin <fadhilin.official@gmail.com>
2026-05-14 22:58:12 +0700

    fix git ignore

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app\\\\result\\\\page.tsx -> canvas-confetti\":{\"id\":\"app\\\\result\\\\page.tsx -> canvas-confetti\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_canvas-confetti_dist_confetti_module_mjs.js\"]}}"
